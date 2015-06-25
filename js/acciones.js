21fcb21fda57f91c58cd71a95bf817cbf363bf1e
AL27F13 <yannnet51@hotmail.com>
2015-06-25 12:05:15 -0500

    uno
    animales

---
+++ js/acciones.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$('#btnanimales').on ('tap' ,function() {
+		navigator.vibrate(2000);
+		$('#btnregreso').on ('tap' ,function() {
+		navigator.vibrate(2000);
+		$('#btngusano').on ('tap' ,function() {
+		navigator.vibrate(2000);
+		$('#btncesta').on ('tap' ,function() {
+		navigator.vibrate(2000);
+		$('#btnsable').on ('tap' ,function() {
+		navigator.vibrate(2000);
+		$('#btndragon').on ('tap' ,function() {
+		navigator.vibrate(2000);
+		$('#btnmedusa').on ('tap' ,function() {
+		navigator.vibrate(2000);
+	});
+	});
+	});
+	});
+});	
+}); 
+});
+});
+});
